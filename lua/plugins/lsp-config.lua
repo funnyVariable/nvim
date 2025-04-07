a69c2a246be30de729fdb437f5133428ab81e892
funnyVariable <uhmdkhms@gmail.com>
2025-04-07 21:39:26 +0300

    vim global

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,7 +17,15 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
       require("lspconfig").ts_ls.setup({})
     end,
   },

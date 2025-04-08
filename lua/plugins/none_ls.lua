9b1842aa9116b949b467cb4cfbf59e55eef4ef1d
funnyVariable <uhmdkhms@gmail.com>
2025-04-08 09:27:19 +0300

    none_ls eslint_d

--- lua/plugins/none_ls.lua
+++ lua/plugins/none_ls.lua
@@ -5,8 +5,9 @@ return {
 
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
+        require("none-ls.diagnostics.eslint_d"),
+        null_ls.builtins.formatting.stylua,
       },
     })
   end,

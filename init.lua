9ba58695c09034f5c69e3ac7ab3e58536a5d7412
funnyVariable <uhmdkhms@gmail.com>
2025-04-08 17:09:06 +0300

    still in eslint_d problem

--- init.lua
+++ init.lua
@@ -23,3 +23,26 @@ vim.cmd("NvimTreeToggle")
 vim.cmd([[highlight NvimTreeNormal guibg=#f2f2f2]])
 
 vim.cmd([[autocmd VimEnter * lua vim.defer_fn(function() vim.cmd('wincmd p') end, 100)]])
+
+local lsp_formatting = function(bufnr)
+    vim.lsp.buf.format({
+        filter = function(client)
+            -- apply whatever logic you want (in this example, we'll only use null-ls)
+            return client.name == "null-ls"
+        end,
+        bufnr = bufnr,
+    })
+end
+
+local on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+                lsp_formatting(bufnr)
+            end,
+        })
+    end
+end

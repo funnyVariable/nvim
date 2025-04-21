5cae44211b54fd7d5f75d9b7a069efd54b54b45d
funnyVariable <uhmdkhms@gmail.com>
2025-04-21 03:07:32 +0300

    React component snippit, Used ctrl-f to move between snippit inserts

--- lua/my-snippets/javascript.lua
+++ lua/my-snippets/javascript.lua
@@ -3,10 +3,26 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
+local extras = require("luasnip.extras")
+local rep = extras.rep
+
 return {
   s("log", {
     t("console.log("),
     i(1, ""),
     t(");"),
+  }),
+
+  s("rfc", {
+    t("function "),
+    i(1, ""),
+    t(" () {"),
+    t({ "", "  return <>" }),
+    i(2, ""),
+    t({ "</>" }),
+    t({ "", "};", "", "" }),
+    t("export default "),
+    rep(1),
+    t(";"),
   }),
 }
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -33,11 +33,17 @@ return {
         },
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-
+          ["<C-f>"] = cmp.mapping(function(fallback)
+            if luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
           ["<C-p>"] = cmp.config.disable, -- Exclude C-p
         }),
         sources = cmp.config.sources({

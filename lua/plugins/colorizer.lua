cb3b0c510be273711735d4dc0abbb5211d66f8e3
funnyVariable <uhmdkhms@gmail.com>
2025-05-13 10:14:16 +0300

    fixed colorizer cmp popup issue

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,10 +1,20 @@
 return {
   "NvChad/nvim-colorizer.lua",
   opts = {
+    filetypes = {
+      "*", -- Highlight all files, but customize some others.
+      cmp_menu = { always_update = true },
+      cmp_docs = { always_update = true },
+    },
     user_default_options = {
       tailwind = true,
       css = true,
-      mode = "background",
+      rgb_fn = true,
+      hsl_fn = true,
+      css_fn = true,
+      mode = "virtualtext",
+      virtualtext = "■",
+      virtualtext_inline = "before",
     },
   },
 }

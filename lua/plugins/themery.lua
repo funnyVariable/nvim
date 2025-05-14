fa8debfd768043a67f7f67e9d4ef77cd9a47ad96
funnyVariable <uhmdkhms@gmail.com>
2025-05-14 07:19:31 +0300

    Nightfox theme

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -9,9 +9,15 @@ require("ibl").setup({
 })
 ]]
 
-local setIblColor = [[
+local setIblColorDark = [[
 vim.api.nvim_set_hl(0, "IblIndent", { fg = "#444444" })
 vim.api.nvim_set_hl(0, "IblScope", { fg = "#888888" })
+require("ibl").setup({ indent = { char = "│", highlight = "IblIndent", }, scope = { enabled = true, highlight = "IblScope", show_start = false, show_end = false, }, })
+]]
+
+local setIblColorLight = [[
+vim.api.nvim_set_hl(0, "IblIndent", { fg = "#dddddd" })
+vim.api.nvim_set_hl(0, "IblScope", { fg = "#aaaaaa" })
 require("ibl").setup({ indent = { char = "│", highlight = "IblIndent", }, scope = { enabled = true, highlight = "IblScope", show_start = false, show_end = false, }, })
 ]]
 
@@ -34,25 +40,67 @@ return {
           name = "vscode Dark",
           colorscheme = "vscode",
           before = dark,
-          after = setIblColor,
+          after = setIblColorDark,
         },
         {
           name = "Kanagawa dragon",
           colorscheme = "kanagawa-dragon",
           before = dark,
-          after = setIblColor,
+          after = setIblColorDark,
         },
         {
           name = "Kanagawa wave",
           colorscheme = "kanagawa-wave",
           before = dark,
-          after = setIblColor,
+          after = setIblColorDark,
         },
         {
           name = "Kanagawa lotus",
           colorscheme = "kanagawa-lotus",
           before = light,
-          after = setIblColor,
+          after = setIblColorLight,
+        },
+        {
+          name = "Nightfox",
+          colorscheme = "nightfox",
+          before = dark,
+          after = setIblColorDark,
+        },
+        {
+          name = "Dayfox",
+          colorscheme = "dayfox",
+          before = light,
+          after = setIblColorLight,
+        },
+        {
+          name = "Dawnfox",
+          colorscheme = "dawnfox",
+          before = light,
+          after = setIblColorLight,
+        },
+        {
+          name = "Duskfox",
+          colorscheme = "duskfox",
+          before = dark,
+          after = setIblColorDark,
+        },
+        {
+          name = "Nordfox",
+          colorscheme = "nordfox",
+          before = dark,
+          after = setIblColorDark,
+        },
+        {
+          name = "Terafox",
+          colorscheme = "terafox",
+          before = dark,
+          after = setIblColorDark,
+        },
+        {
+          name = "Carbonfox",
+          colorscheme = "carbonfox",
+          before = dark,
+          after = setIblColorDark,
         },
       },
       livePreview = true,

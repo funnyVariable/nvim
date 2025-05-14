c62895c8e1275b993195517114d0f0ef0fc084c3
funnyVariable <uhmdkhms@gmail.com>
2025-05-14 07:29:34 +0300

    removed window border from cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -27,10 +27,10 @@ return {
             luasnip.lsp_expand(args.body)
           end,
         },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
+        -- window = {
+        --   completion = cmp.config.window.bordered(false),
+        --   documentation = cmp.config.window.bordered(false),
+        -- },
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           -- ["<C-f>"] = cmp.mapping.scroll_docs(4),

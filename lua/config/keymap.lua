cf17b5733ea99c1577c64fca608ec06e27435fe7
funnyVariable <uhmdkhms@gmail.com>
2025-06-12 01:28:10 +0300

    keymap barbar

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -103,27 +103,11 @@ vim.keymap.set("n", "<leader>G", "<Cmd>Gitsigns reset_hunk<CR>")
 ---- Buffers ----
 
 -- Move to previous/next
-vim.keymap.set("n", "<A-h>", "<Cmd>BufferPrevious<CR>")
-vim.keymap.set("n", "<A-l>", "<Cmd>BufferNext<CR>")
-
--- Re-order to previous/next
-vim.keymap.set("n", "<A-a>", "<Cmd>BufferMovePrevious<CR>")
-vim.keymap.set("n", "<A-d>", "<Cmd>BufferMoveNext<CR>")
-
--- Goto buffer in position...
-vim.keymap.set("n", "<A-1>", "<Cmd>BufferGoto 1<CR>")
-vim.keymap.set("n", "<A-2>", "<Cmd>BufferGoto 2<CR>")
-vim.keymap.set("n", "<A-3>", "<Cmd>BufferGoto 3<CR>")
-vim.keymap.set("n", "<A-4>", "<Cmd>BufferGoto 4<CR>")
-vim.keymap.set("n", "<A-5>", "<Cmd>BufferGoto 5<CR>")
-vim.keymap.set("n", "<A-6>", "<Cmd>BufferGoto 6<CR>")
-vim.keymap.set("n", "<A-7>", "<Cmd>BufferGoto 7<CR>")
-vim.keymap.set("n", "<A-8>", "<Cmd>BufferGoto 8<CR>")
-vim.keymap.set("n", "<A-9>", "<Cmd>BufferGoto 9<CR>")
-vim.keymap.set("n", "<A-0>", "<Cmd>BufferLast<CR>")
+vim.keymap.set("n", "<A-h>", "<Cmd>bprevious<CR>")
+vim.keymap.set("n", "<A-l>", "<Cmd>bnext<CR>")
 
 -- Close buffer
-vim.keymap.set("n", "<A-c>", "<Cmd>BufferClose<CR>")
+vim.keymap.set("n", "<A-c>", "<Cmd>bdelete<CR>")
 
 ---- Buffers End ----
 
--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,1 +1,28 @@
-return { "romgrk/barbar.nvim" }
+return {
+  "romgrk/barbar.nvim",
+  enabled = false,
+  config = function()
+    -- Move to previous/next
+    vim.keymap.set("n", "<A-h>", "<Cmd>BufferPrevious<CR>")
+    vim.keymap.set("n", "<A-l>", "<Cmd>BufferNext<CR>")
+
+    -- Re-order to previous/next
+    vim.keymap.set("n", "<A-a>", "<Cmd>BufferMovePrevious<CR>")
+    vim.keymap.set("n", "<A-d>", "<Cmd>BufferMoveNext<CR>")
+
+    -- Goto buffer in position...
+    vim.keymap.set("n", "<A-1>", "<Cmd>BufferGoto 1<CR>")
+    vim.keymap.set("n", "<A-2>", "<Cmd>BufferGoto 2<CR>")
+    vim.keymap.set("n", "<A-3>", "<Cmd>BufferGoto 3<CR>")
+    vim.keymap.set("n", "<A-4>", "<Cmd>BufferGoto 4<CR>")
+    vim.keymap.set("n", "<A-5>", "<Cmd>BufferGoto 5<CR>")
+    vim.keymap.set("n", "<A-6>", "<Cmd>BufferGoto 6<CR>")
+    vim.keymap.set("n", "<A-7>", "<Cmd>BufferGoto 7<CR>")
+    vim.keymap.set("n", "<A-8>", "<Cmd>BufferGoto 8<CR>")
+    vim.keymap.set("n", "<A-9>", "<Cmd>BufferGoto 9<CR>")
+    vim.keymap.set("n", "<A-0>", "<Cmd>BufferLast<CR>")
+
+    -- Close buffer
+    vim.keymap.set("n", "<A-c>", "<Cmd>BufferClose<CR>")
+  end,
+}
